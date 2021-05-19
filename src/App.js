7174cd191182e5a136bed80ea078f51a5fa0db7c
huseyin gulcu <huseyingulcuu@gmail.com>
2021-05-19 15:42:37 +0300

    to highlighted style, i changed Link to NavLink to utilize its activeClassName props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import {Route} from  'react-router-dom'
-import Products from './pages/Products';
-import Welcome from './pages/Welcome';
+import { Route } from "react-router-dom";
+import Products from "./pages/Products";
+
+
+import Welcome from "./pages/Welcome";
+import Header from './components/Header';
 function App() {
   return (
     <div>
-      <Route path="/welcome" >
-        <Welcome />
-      </Route>
-      <Route path="/products" >
-        <Products />
-      </Route>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
